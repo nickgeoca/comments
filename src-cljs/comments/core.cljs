7c5cb6d84a62bb8546eb1479997867b6ffc6a367
Nick Geoca <nickgeoca@gmail.com>
2014-09-25 23:46:36 -0500

    playing around w/ html/cljs to get it working, but not

--- src-cljs/comments/core.cljs
+++ src-cljs/comments/core.cljs
@@ -14,8 +14,13 @@
   (action)))
 (def ws-url (str "ws://" (.-host js/location) "/comments/ws"))
 (def socket (js/WebSocket. ws-url))
-(defn send-message [] (.send socket {:msg (.-value (.getElementById js/document "comment")) }))
-(.preventDefault (.onClick (.getElementById js/document "send") send-message))
+(defn send-message []
+;;  (let [message (.-value (.getElementById js/document "comment"))])
+  (.send socket "hey!!!!!"))
+;;js/document.getElementById ("send").onClick
+;;js/document.getElementById ("send").addEventListener
+;; ("send").addEventListener('click', function(){ socket.send("this is data!") })
+(.addEventListener (.getElementById js/document "send") 'click' send-message)
 (fw/watch-and-reload
   :websocket-url   "ws://localhost:3449/figwheel-ws"
   :jsload-callback (fn [] (print "reloaded")))
